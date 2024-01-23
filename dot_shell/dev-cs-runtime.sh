91581fb6036ea01925149ad74dd27e3513b31213
aborgardt <a.borgardt@sdzecom.de>
2024-01-23 21:59:05 +0100

    Update .shell/dev-cs-runtime.sh

--- dot_shell/dev-cs-runtime.sh
+++ dot_shell/dev-cs-runtime.sh
@@ -7,5 +7,5 @@ dev-cs-runtime ()
   user=${2-www-data}
   version=${1-23.0.0}
  
-  docker run --user $user -it -v ./ressources:/opt/scripts --entrypoint /bin/bash registry.sdzecom-internal.de/contentserv_replica/pxc/pim:$version
+  docker run --rm --user $user -it -v ./resources:/opt/scripts --entrypoint /bin/bash registry.sdzecom-internal.de/contentserv_replica/pxc/pim:$version
 }
