23beaedd1bbd7dd8a11b25ae51cc28983f5c8aa7
aborgardt <a.borgardt@sdzecom.de>
2024-01-26 08:07:23 +0100

    Update .shell/gitea-act-runner.sh

--- dot_shell/gitea-act-runner.sh
+++ dot_shell/gitea-act-runner.sh
@@ -1,6 +1,9 @@
 function runAct()
 {
-  if [[ -f ~/.config/act.env ]]; then	
-    act --secret-file ~/.config/act.env -W $1
+  if [[ ! -f ~/.config/act.env ]]; then
+    echo "You have no act.env in your .config folder"
+    return 1
   fi
+
+  act --secret-file ~/.config/act.env -W $1
 }
